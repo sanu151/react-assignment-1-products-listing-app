ed9fa92afe5e7d0800faebe1ee4c049cff909d07
Supriyo Das <das.supriyo07@gmail.com>
2024-05-24 05:23:44 +0530

    Assignment 1

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,20 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable */
 import React from 'react';
 
-const Product = () => {
+function Product(props) {
+  const { product } = props;
   return (
-      <article className="product">
-      <img src="" alt="" />
+    <article className="product">
+      <img src={product.image} alt={product.id} />
       <div className="product__details">
-        <h4 className="product__title">product title</h4>
-        <p>Price: $ product price</p>
-        <p>Rating: product rating rate/5</p>
-        <p className="product__desc">Description: product.description</p>
+        <h4 className="product__title">{product.title}</h4>
+        <p>Price: $ {product.price}</p>
+        <p>Rating: product rating {product.rate}/5</p>
+        <p className="product__desc">Description: {product.description}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
     </article>
   );
-};
+}
 
 export default Product;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,16 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable */
+
+import Product from './Product';
+
+function Products(props) {
+  const { products } = props;
+  return (
+    <div className="products">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
+
+export default Products;
